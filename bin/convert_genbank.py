a41f01296b05a3e2487512338310346807af864b
jennycmuscat <jennycmuscat@gmail.com>
2024-07-11 11:27:42 +0100

    Update copyright

--- bin/convert_genbank.py
+++ bin/convert_genbank.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-# Copyright 2021 Edinburgh Genome Foundry, University of Edinburgh
+# Copyright 2024 Edinburgh Genome Foundry, University of Edinburgh
 #
-# This file is part of Sequeduct.
+# This file is part of Sequeduct Methyl.
 #
-# Sequeduct is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# Sequeduct Methyl is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 #
-# Sequeduct is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# Sequeduct Methyl is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License along with Sequeduct. If not, see <https:www.gnu.org/licenses/>.
 
